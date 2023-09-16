23faaa0cccbb2a28249a453d6c565425abfcdfae
bvguchefnjimwondhxbygrfhuedijm <143262150+bvguchefnjimwondhxbygrfhuedijm@users.noreply.github.com>
2023-09-16 08:00:07 -0400

    Create uv.config.js

---
+++ uv_edit/uv.config.js
@@ -0,0 +1,12 @@
+/*global Ultraviolet*/
+self.__uv$config = {
+    prefix: '/uv_edit/education/',
+    bare: 'https://8vk2d6-6987.csb.app/',
+    encodeUrl: Ultraviolet.codec.xor.encode,
+    decodeUrl: Ultraviolet.codec.xor.decode,
+    handler: '/educationhahaha/uv_edit/uv.handler.js',
+    client: '/educationhahaha/uv_edit/uv.client.js',
+    bundle: '/educationhahaha/uv_edit/uv.bundle.js',
+    config: '/educationhahaha/uv_edit/uv.config.js',
+    sw: '/educationhahaha/uv_edit/uv.sw.js',
+};
